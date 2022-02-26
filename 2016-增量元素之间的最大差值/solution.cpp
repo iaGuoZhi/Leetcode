ce76f625eb04eed074e5fb5c0c23c379bcf3a31a
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-02-26 10:49:24 +0800

    day-4

--- 2016-增量元素之间的最大差值/solution.cpp
+++ 2016-增量元素之间的最大差值/solution.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
         int res = -1;
-        int i = 0, j = 1;
-         //    int left_min = INT_MAX, right_max = INT_MIN;
+        int left_min_idx = -1;
 
         if(nums.size() <= 1)
             return res;
-        for(i; i < nums.size(); ++i){
-            for(j = i + 1; j < nums.size(); ++j){
-                if(nums[j] - nums[i] > 0 && nums[j] - nums[i] > res){
-                    res = nums[j] - nums[i];
+        for(int i = 0; i < nums.size() - 1; ++i){
+            if(left_min_idx < 0 || nums[i] < nums[left_min_idx]){
+                for(int j = i + 1; j < nums.size(); ++j){
+                    if(nums[j] - nums[i] > 0 && nums[j] - nums[i] > res){
+                        res = nums[j] - nums[i];
+                    }
                 }
+                left_min_idx = i;
             }
         }
 
