c5a0020482a0ecf9941ea97cc2d76358ec5bf78c
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-04-12 08:51:28 +0800

    day-21.25

--- daily-problems/806-写字符串需要的行数/number_of_lines/src/main.rs
+++ daily-problems/806-写字符串需要的行数/number_of_lines/src/main.rs
@@ -1,25 +1,18 @@
 impl Solution {
     pub fn number_of_lines(widths: Vec<i32>, s: String) -> Vec<i32> {
-        let mut number_line : i32 = 0;
+        let mut number_line : i32 = 1;
         let mut cur_width : i32 = 0;
-        let mut remain_width : i32 = 0;
-        let mut res : Vec<i32> = Vec::new();
-        let base : i32 = stoi('a');
 
-        for c in s.split_whitespace() {
-            let idx = stoi(c) - base;
+        for c in s.as_bytes() {
+            let idx : i32 = c - 97;
             if cur_width + widths[idx as usize] <= 100 {
-                cur_width = cur_width + widths[idx as usize];
+                cur_width += widths[idx as usize];
             } else {
-                remain_width = cur_width;
-                cur_width = 0;
-                number_line = number_line + 1;
+                cur_width = widths[idx as usize];
+                number_line += 1;
             }
         }
 
-        res.push(number_line);
-        res.push(remain_width);
-
-        res
+        vec![number_line, cur_width]
     }
 }
