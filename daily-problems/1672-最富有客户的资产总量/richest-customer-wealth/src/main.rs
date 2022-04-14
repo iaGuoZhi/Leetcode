d00ef7c20caabbb85bb0c559564e3ec995fdb184
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-04-14 21:04:59 +0800

    day-21.27

--- daily-problems/1672-最富有客户的资产总量/richest-customer-wealth/src/main.rs
+++ daily-problems/1672-最富有客户的资产总量/richest-customer-wealth/src/main.rs
@@ -1,17 +1,5 @@
 impl Solution {
     pub fn maximum_wealth(mut accounts: Vec<Vec<i32>>) -> i32 {
-        let mut max_wealth : i32 = 0;
-        for man in accounts {
-            let mut money = 0;
-            for m_bank in man {
-                money += m_bank;
-            }
-            if money > max_wealth {
-                max_wealth = money;
-            }
-        }
-
-
-        return max_wealth;
+        accounts.iter().map({|x| x.iter().sum()).max().unwrap()
     }
 }
