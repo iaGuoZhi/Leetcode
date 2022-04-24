aa7c361cd3375a7b0e9cc0ed6cca2670c1a262fb
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-04-24 15:41:18 +0800

    day-21.37

--- daily-problems/1672-最富有客户的资产总量/richest-customer-wealth/src/main.rs
+++ daily-problems/1672-最富有客户的资产总量/richest-customer-wealth/src/main.rs
@@ -1,5 +1,5 @@
 impl Solution {
     pub fn maximum_wealth(mut accounts: Vec<Vec<i32>>) -> i32 {
-        accounts.iter().map({|x| x.iter().sum()).max().unwrap()
+        accounts.iter().map(|x| x.iter().sum()).max().unwrap()
     }
 }
