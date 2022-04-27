53f9263f0d8be5a42efee9693de3051ece7dd550
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-04-27 14:07:35 +0800

    day-21.40

---
+++ daily-problems/398-随机数索引/random-pick-index/target/debug/deps/random_pick_index-66b8674069fca71b.d
@@ -0,0 +1,5 @@
+/Users/zhiguo/Projects/Leetcode/daily-problems/398-随机数索引/random-pick-index/target/debug/deps/random_pick_index-66b8674069fca71b: src/main.rs
+
+/Users/zhiguo/Projects/Leetcode/daily-problems/398-随机数索引/random-pick-index/target/debug/deps/random_pick_index-66b8674069fca71b.d: src/main.rs
+
+src/main.rs:
